8615115198ab4a910273dcc06b020979c8035cc5
Hatim Rajab <altorbed7@gmail.com>
2025-02-23 21:16:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function timer() {
+  // variables declaration
+
+  var sec = document.querySelector(".sec");
+  var min = document.querySelector(".min");
+  var hour = document.querySelector(".hour");
+  var start = document.querySelector(".start");
+  var reset = document.querySelector(".reset");
+
+  var s = 0;
+  var m = 0;
+  var h = 0;
+
+  // setInterval function
+
+  function tim() {
+    setInterval(function timStart() {
+      s++;
+      if (s === 60) {
+        s = 0;
+        m++;
+        if (m === 60) {
+          m = 0;
+          h++;
+        }
+      }
+      sec.innerHTML = s;
+      min.innerHTML = m;
+      hour.innerHTML = h;
+    }, 1000);
+  }
+
+  // Buttons Events
+
+  start.onclick  = tim
+  reset.onclick = function () {
+    s = 0;
+    m = 0;
+    h =0
+  }
+}
+
+
+// Calling Function
+
+timer();
